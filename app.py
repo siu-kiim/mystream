53bebfd865f9607e526d452ed47048e72cc4a708
siu-kiim <126744648+siu-kiim@users.noreply.github.com>
2023-05-16 11:37:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st 
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
-st.sidebar.title("This is written inside the sidebar")
-st.sidebar.button("Click")
-st.sidebar.radio("Pick your gender",["Male", "Female"])
+rand = np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15) #,color="pink"
+st.pyplot(fig)
