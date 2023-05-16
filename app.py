431a7f525ced98b764173cd98b57cf464b17848e
siu-kiim <126744648+siu-kiim@users.noreply.github.com>
2023-05-16 11:30:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
-st.sidemar.title("This is written inside the sidebar")
+st.sidebar.title("This is written inside the sidebar")
+st.sidebar.button("Click")
+st.sidebar.radio("Pick your gender",["Male", "Female"])
